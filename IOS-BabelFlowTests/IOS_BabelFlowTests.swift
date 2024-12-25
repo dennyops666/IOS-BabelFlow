be4a9e02f4c7c91cf8bc530612ff6e847f3bba5f
dennyops <jeywork666@gmail.com>
2024-12-25 19:55:24 +0800

    first commit

---
+++ IOS-BabelFlowTests/IOS_BabelFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_BabelFlowTests.swift
+//  IOS-BabelFlowTests
+//
+//  Created by Django on 12/25/24.
+//
+
+import Testing
+@testable import IOS_BabelFlow
+
+struct IOS_BabelFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
